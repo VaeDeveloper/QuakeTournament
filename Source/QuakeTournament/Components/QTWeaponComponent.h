b5c0af26199892384f9bac46f793617a0d72395b
Dmitriy Alexandrov <104723129+VaeDeveloper@users.noreply.github.com>
2025-03-02 19:52:22 +0300

    delete test property in weaponcomponent class

--- Source/QuakeTournament/Components/QTWeaponComponent.h
+++ Source/QuakeTournament/Components/QTWeaponComponent.h
@@ -20,8 +20,4 @@ protected:
 
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-	UPROPERTY(EditAnywhere, Category = "WeaponComponent|Weapons")
-	float Test = 1.0f;
-
 };
